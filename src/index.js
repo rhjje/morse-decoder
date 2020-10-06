cc08a40279bc791ba6b1571516dc642fa870e1b4
rhjje <numelen@gmail.com>
2020-10-07 00:40:48 +0300

    feat: done Morse decoder task

--- src/index.js
+++ src/index.js
@@ -35,12 +35,36 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '':  ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let code = [];
+    let morseCode = [];
+
+    for (let i = 0; i < expr.length; i+= 10) {
+        code.push(expr.slice(i, i + 10));
+    }
+
+    for (let i = 0; i < code.length; i++) {
+        let temp = '';
+        for (let j = 0; j < code[i].length; j += 2) {
+            if (code[i].slice(j, j + 2) === '00') {
+                temp += '';
+            } else if (code[i].slice(j, j + 2) === '10') {
+                temp += '.';
+            } else if (code[i].slice(j, j + 2) === '11') {
+                temp += '-';
+            }
+        }
+        morseCode.push(temp);
+        temp = '';
+    }
+
+    return morseCode.map(item => MORSE_TABLE[item]).join('');
 }
 
 module.exports = {
     decode
 }
+
